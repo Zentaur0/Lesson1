d5fce99c280274c4affd75480598c40d6f35b337
Anton <freshbreathe@gmail.com>
2020-12-14 23:56:11 +0300

    lesson1

--- WelcomeToSwift/main.swift
+++ WelcomeToSwift/main.swift
@@ -7,5 +7,19 @@
 
 import Foundation
 
-print("Hello, World!")
+// квадратное уровнение x2 − y = z
+var x: Double = 5
+var y: Double = 10
+var z = pow(x, 2) - y
 
+// треугольник
+let a: Float = 10
+let b: Float = 6
+let c = sqrt(pow(a, 2) + pow(b, 2)) // гипотенуза треугольника
+let p = a + b + c // периметр треугольника
+let s = a * b / 2 // площадь треугольника
+
+print(z)
+print("Гипотенуза треугольника = " + String(c))
+print("Периметр треугольника = " + String(p))
+print("Площадь треугольника = " + String(s))
